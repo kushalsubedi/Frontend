e01486ca1650b95c93336c2328b507e6af672fd0
kushalsubedi <kushalsubedi55@gmail.com>
2025-07-02 14:44:43 +0545

    fix: semantic versioning check

--- .github/actions/version-bump/bump.sh
+++ .github/actions/version-bump/bump.sh
@@ -2,19 +2,13 @@
 set -e
 
 if [ -z "$GITHUB_OUTPUT" ]; then
-  export GITHUB_OUTPUT="/tmp/github_output.txt"
-  echo "Running locally, writing outputs to $GITHUB_OUTPUT"
+  echo " GITHUB_OUTPUT is not set"
+  exit 1
 fi
-
-write_output() {
-  if [ -n "$GITHUB_OUTPUT" ]; then
-    echo "$1" >> "$GITHUB_OUTPUT"
-  fi
-}
 
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1` 2>/dev/null || echo "v0.0.0")
 commit_msg=$(git log -1 --pretty=%s)
-echo "HI "
+echo "HI"
 version=${latest_tag#v}
 major=$(echo "$version" | cut -d. -f1)
 minor=$(echo "$version" | cut -d. -f2)
@@ -27,10 +21,11 @@ elif [[ "$commit_msg" == feat:* ]]; then
 elif [[ "$commit_msg" == fix:* ]]; then
     patch=$((patch+1))
 else
-    write_output "skip=true"
+    echo "skip=true" >> "$GITHUB_OUTPUT"
     exit 0
 fi
 
 new_tag="v$major.$minor.$patch"
-write_output "new_tag=$new_tag"
-write_output "skip=false"
+echo "new_tag=$new_tag" >> "$GITHUB_OUTPUT"
+echo "skip=false" >> "$GITHUB_OUTPUT"
+
